66271c3aa139dbdb5f296321a63899fe152ce735
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2022-04-21 23:44:45 -0400

    Sorting Algorithms & DCLinkedList Done

---
+++ DataStructures/SortingAlgorithms/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.pbHistogram = new System.Windows.Forms.PictureBox();
+            this.btnBubbleSort = new System.Windows.Forms.Button();
+            this.btnInsertionSort = new System.Windows.Forms.Button();
+            this.btnQuickSort = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHistogram)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.Location = new System.Drawing.Point(57, 12);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(500, 40);
+            this.btnShuffle.TabIndex = 1;
+            this.btnShuffle.Text = "SHUFFLE";
+            this.btnShuffle.UseVisualStyleBackColor = true;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+            // 
+            // pbHistogram
+            // 
+            this.pbHistogram.Location = new System.Drawing.Point(57, 104);
+            this.pbHistogram.Name = "pbHistogram";
+            this.pbHistogram.Size = new System.Drawing.Size(500, 230);
+            this.pbHistogram.TabIndex = 1;
+            this.pbHistogram.TabStop = false;
+            this.pbHistogram.Paint += new System.Windows.Forms.PaintEventHandler(this.pbHistogram_Paint);
+            // 
+            // btnBubbleSort
+            // 
+            this.btnBubbleSort.Location = new System.Drawing.Point(57, 56);
+            this.btnBubbleSort.Name = "btnBubbleSort";
+            this.btnBubbleSort.Size = new System.Drawing.Size(150, 40);
+            this.btnBubbleSort.TabIndex = 2;
+            this.btnBubbleSort.Text = "BUBBLE SORT";
+            this.btnBubbleSort.UseVisualStyleBackColor = true;
+            this.btnBubbleSort.Click += new System.EventHandler(this.btnBubbleSort_Click);
+            // 
+            // btnInsertionSort
+            // 
+            this.btnInsertionSort.Location = new System.Drawing.Point(407, 56);
+            this.btnInsertionSort.Name = "btnInsertionSort";
+            this.btnInsertionSort.Size = new System.Drawing.Size(150, 40);
+            this.btnInsertionSort.TabIndex = 4;
+            this.btnInsertionSort.Text = "INSERTION SORT";
+            this.btnInsertionSort.UseVisualStyleBackColor = true;
+            this.btnInsertionSort.Click += new System.EventHandler(this.btnInsertionSort_Click);
+            // 
+            // btnQuickSort
+            // 
+            this.btnQuickSort.Location = new System.Drawing.Point(232, 56);
+            this.btnQuickSort.Name = "btnQuickSort";
+            this.btnQuickSort.Size = new System.Drawing.Size(150, 40);
+            this.btnQuickSort.TabIndex = 3;
+            this.btnQuickSort.Text = "QUICK SORT";
+            this.btnQuickSort.UseVisualStyleBackColor = true;
+            this.btnQuickSort.Click += new System.EventHandler(this.btnQuickSort_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblTime.Location = new System.Drawing.Point(54, 342);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 17);
+            this.lblTime.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 368);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnQuickSort);
+            this.Controls.Add(this.btnInsertionSort);
+            this.Controls.Add(this.btnBubbleSort);
+            this.Controls.Add(this.pbHistogram);
+            this.Controls.Add(this.btnShuffle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbHistogram)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.PictureBox pbHistogram;
+        private System.Windows.Forms.Button btnBubbleSort;
+        private System.Windows.Forms.Button btnInsertionSort;
+        private System.Windows.Forms.Button btnQuickSort;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ DataStructures/SortingAlgorithms/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        private int[] array = new int[100];
+        private bool shuffling = false;
+
+        private int workingIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            DisableControls();
+            Cursor = Cursors.WaitCursor;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            shuffling = true;
+            var random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomIndex = random.Next(0, array.Length);
+
+                int temp = array[i];
+                array[i] = array[randomIndex];
+                array[randomIndex] = temp;
+
+                Refresh();
+            }
+            shuffling = false;
+
+            Cursor = Cursors.Default;
+            EnableControls();
+        }
+
+        private void btnBubbleSort_Click(object sender, EventArgs e)
+        {
+            DisableControls();
+            Cursor = Cursors.WaitCursor;
+
+            DateTime start = DateTime.Now;
+
+            BubbleSort(array);
+            workingIndex = -1;
+            Refresh();
+
+            var elapsed = DateTime.Now - start;
+            lblTime.Text = $"Time elapsed: {elapsed}";
+
+            Cursor = Cursors.Default;
+            EnableControls();
+        }
+
+        private void btnInsertionSort_Click(object sender, EventArgs e)
+        {
+            DisableControls();
+            Cursor = Cursors.WaitCursor;
+
+            DateTime start = DateTime.Now;
+
+            InsertionSort(array);
+            workingIndex = -1;
+            Refresh();
+
+            var elapsed = DateTime.Now - start;
+            lblTime.Text = $"Time elapsed: {elapsed}";
+
+            Cursor = Cursors.Default;
+            EnableControls();
+        }
+
+        private void btnQuickSort_Click(object sender, EventArgs e)
+        {
+            DisableControls();
+            Cursor = Cursors.WaitCursor;
+
+            DateTime start = DateTime.Now;
+
+            QuickSort(array, 0, array.Length - 1);
+            workingIndex = -1;
+            Refresh();
+
+            var elapsed = DateTime.Now - start;
+            lblTime.Text = $"Time elapsed: {elapsed}";
+
+            Cursor = Cursors.Default;
+            EnableControls();
+        }
+
+        private void pbHistogram_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var bounds = new Rectangle(i * 5, 230 - (array[i] * 2), 4, array[i] * 2);
+                var pen = new Pen(Color.Black, 1);
+                var brush = new SolidBrush(Color.DarkGray);
+                e.Graphics.DrawRectangle(pen, bounds);
+                e.Graphics.FillRectangle(brush, bounds);
+
+                bool notSorted = array[i] == i + 1;
+                if (notSorted && !shuffling)
+                {
+                    brush = new SolidBrush(Color.Green);
+                    e.Graphics.FillRectangle(brush, bounds);
+                }
+
+                if (i == workingIndex)
+                {
+                    brush = new SolidBrush(Color.Red);
+                    e.Graphics.FillRectangle(brush, bounds);
+                }
+            }
+        }
+
+        private void BubbleSort(int[] array)
+        {
+            for (int i = 0; i <= array.Length - 2; i++)
+            {
+                for (int j = 0; j <= array.Length - 2; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        workingIndex = j;
+                        var tmp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = tmp;
+                        Refresh();
+                    }
+                }
+            }
+        }
+
+        private void InsertionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int item = array[i];
+                int index = i;
+                while (index > 0 && array[index - 1] > item)
+                {
+                    workingIndex = index;
+                    array[index] = array[index - 1];
+                    index--;
+                    Refresh();
+                }
+                array[index] = item;
+            }
+        }
+
+        private void QuickSort(int[] arr, int start, int end)
+        {
+            int i;
+            if (start < end)
+            {
+                i = Partition(arr, start, end);
+
+                QuickSort(arr, start, i - 1);
+                QuickSort(arr, i + 1, end);
+            }
+        }
+
+        private int Partition(int[] arr, int start, int end)
+        {
+            int temp;
+            int p = arr[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end - 1; j++)
+            {
+                if (arr[j] <= p)
+                {
+                    i++;
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    workingIndex = j;
+                    Refresh();
+                }
+            }
+
+            temp = arr[i + 1];
+            arr[i + 1] = arr[end];
+            arr[end] = temp;
+            return i + 1;
+        }
+
+        private void DisableControls()
+        {
+            btnShuffle.Enabled = false;
+            btnQuickSort.Enabled = false;
+            btnInsertionSort.Enabled = false;
+            btnBubbleSort.Enabled = false;
+        }
+
+        private void EnableControls()
+        {
+            btnShuffle.Enabled = true;
+            btnQuickSort.Enabled = true;
+            btnInsertionSort.Enabled = true;
+            btnBubbleSort.Enabled = true;
+        }
+    }
+}
